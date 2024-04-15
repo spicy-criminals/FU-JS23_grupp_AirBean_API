79ec7db3c5b6a4a684e9d259b123c5b791b39e87
Marcus Widen <hellowiden@gmail.com>
2024-04-15 22:17:42 +0200

    comment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-// REQUESTS
-
-// get-request föratt visa user-sidan
+// GET request to display the user page
 router.get("/", (req, res) => {
-  res.send({ data: "please sign up" });
+  res.send({ message: "Please sign up" });
 });
 
-//post-request föratt skapa användare
+// POST request to create a new user
 router.post("/", (req, res) => {
-  res.send({ data: "created" });
+  res.send({ message: "User created" });
 });
 
+// PUT request to update user information
 router.put("/", (req, res) => {
-  res.send({ data: "updated" });
+  res.send({ message: "User information updated" });
 });
 
+// DELETE request to delete a user
 router.delete("/", (req, res) => {
-  res.send({ data: "deleted" });
+  res.send({ message: "User deleted" });
 });
 
 module.exports = router;
