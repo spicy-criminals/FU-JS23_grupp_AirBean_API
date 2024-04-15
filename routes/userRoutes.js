d3dd6fb76fd4d2c4d67e86577f1b241799d6d412
annathunberg <anna.thunberg1@gmail.com>
2024-04-15 22:50:53 +0200

    functionality for creating new user, save users in database, uuid and bcrypt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../database");
+const { createUser } = require("../controllers/UserController");
+const { validateNewUser, validate } = require("../validators/userValidators");
 
-// REQUESTS
+/*
+GET /users för att hämta alla användare.
+POST /users för att skapa en ny användare.
+GET /users/:id för att hämta en specifik användare.
+PUT /users/:id för att uppdatera en specifik användare.
+DELETE /users/:id för att radera en specifik användare.
+*/
 
-// get-request föratt visa user-sidan
-router.get("/", (req, res) => {
-  res.send({ data: "please sign up" });
+///////// REQUESTS //////////
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await db.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ error: "Kunde inte hämta användare" });
+  }
 });
 
 //post-request föratt skapa användare
-router.post("/", (req, res) => {
-  res.send({ data: "created" });
-});
+router.post("/", validateNewUser(), validate, createUser);
 
+// put
 router.put("/", (req, res) => {
   res.send({ data: "updated" });
 });
 
-router.delete("/", (req, res) => {
-  res.send({ data: "deleted" });
+//delete
+router.delete("/", async (req, res) => {
+  try {
+    const result = await db.remove({}, { multi: true });
+    res.send({ message: "Alla användare raderade", deletedCount: result });
+  } catch (error) {
+    res.status(500).send({ error: "Kunde inte radera användare" });
+  }
+  // res.send({ data: "deleted" });
 });
 
 module.exports = router;
