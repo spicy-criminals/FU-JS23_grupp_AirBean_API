e082a106d2d6ba3df026b4959d8f9213f71f0a8e
annathunberg <anna.thunberg1@gmail.com>
2024-04-16 22:30:16 +0200

    fixes in userroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,76 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../database");
-const { createUser } = require("../controllers/UserController");
-const { validateNewUser, validate } = require("../validators/userValidators");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-
-const JWT_SECRET = "your_secret_key_here"; // Use a stronger secret key
+const { createUser } = require("../controllers/UserController");
+const { validateNewUser, validate } = require("../validators/userValidators");
 
 ///////// REQUESTS //////////
 
-// Get all users
+///// GET USERS /////
 router.get("/", async (req, res) => {
   try {
     const users = await db.find({});
     res.send(users);
   } catch (error) {
-    res.status(500).send({ error: "Could not retrieve users" });
+    res.status(404).send({ error: "Could not find any users :(" });
   }
 });
 
-// Create a new user
-router.post("/", validateNewUser(), validate, createUser);
+////// SIGN UP ///////
+router.get("/signup", (req, res) => {
+  res.send({ message: "Make a post-request to sign up! :)" });
+});
+router.post("/signup", validateNewUser(), validate, createUser);
 
-// Retrieve a specific user by ID
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await db.findOne({ _id: req.params.id });
-    if (user) {
-      res.send(user);
-    } else {
-      res.status(404).send({ error: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).send({ error: "Could not retrieve user" });
-  }
+///////// LOGIN ////////
+
+router.get("/login", (req, res) => {
+  res.send({ message: "Make a post-request to login up! :)" });
 });
 
-// Update a specific user by ID
-router.put("/:id", validateNewUser(), validate, async (req, res) => {
-  try {
-    const updateData = req.body;
-    const result = await db.update(
-      { _id: req.params.id },
-      { $set: updateData },
-      { returnUpdatedDocs: true }
-    );
-    if (result.nModified > 0) {
-      res.send({ message: "User information updated", userId: req.params.id });
-    } else {
-      res.status(404).send({ error: "No user found for update" });
-    }
-  } catch (error) {
-    res.status(500).send({ error: "Could not update user" });
-  }
-});
-
-// Delete a specific user by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const result = await db.remove({ _id: req.params.id }, {});
-    if (result > 0) {
-      res.send({ message: "User deleted", userId: req.params.id });
-    } else {
-      res.status(404).send({ error: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).send({ error: "Could not delete user" });
-  }
-});
-
-// User login
 router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -93,6 +52,43 @@ router.post("/login", async (req, res) => {
     res.status(500).send({ error: "Could not log in" });
   }
 });
+
+// get a specific user
+router.get("/user/:username", async (req, res) => {
+  // const userId = req.params.userId;
+  const username = req.params.username;
+
+  try {
+    const user = await db.findOne({ username: username });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ error: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).send({ error: "Server error" });
+  }
+});
+
+// Delete a specific user
+router.delete("/user/:username", async (req, res) => {
+  // const userId = req.params.userId;
+  const username = req.params.username;
+
+  try {
+    const result = await db.remove({ username: username }, {});
+    console.log("hello");
+    if (result > 0) {
+      res.send({ message: "User deleted", username: username });
+    } else {
+      res.status(404).send({ error: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).send({ error: "Server error" });
+  }
+});
+
+////////// AUTH ///////////
 
 // Token authentication middleware
 const authenticate = (req, res, next) => {
