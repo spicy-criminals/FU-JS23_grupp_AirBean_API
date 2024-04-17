35b0b2d54fdec4d92afc884f840de02f5845a717
Marcus Widen <hellowiden@gmail.com>
2024-04-17 13:04:20 +0200

    add comments

--- database.js
+++ database.js
@@ -1,3 +1,13 @@
+/*
+ 
+Summary:
+ * This code initializes a NeDB datastore instance, leveraging the 'nedb-promise' module for asynchronous operations.
+ * It sets up the database with default options, including the filename and autoload settings. The exported NeDB
+ * instance is accessible for use in other modules.
+ * NeDB is a lightweight, file-based embedded database for Node.js, suitable for small to medium-sized datasets.
+
+*/
+
 const Datastore = require("nedb-promise");
 
 const db = new Datastore({
