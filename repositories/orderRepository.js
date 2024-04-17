b48c753c1ded6ec5ba8176dd2c6142342a54325d
Marcus Widen <hellowiden@gmail.com>
2024-04-17 13:42:00 +0200

    add comments

--- repositories/orderRepository.js
+++ repositories/orderRepository.js
@@ -1,3 +1,18 @@
+/* 
+This code sets up a connection to a NeDB database stored in "./orders.db" file.
+It provides functions to create orders, retrieve ongoing orders based on the current time, and retrieve order history for a specific user.
+
+createOrder(userId, productId, price): 
+    - Creates a new order with the provided user ID, product ID, and price.
+    - Generates order and delivery dates and inserts the order into the database.
+
+getOngoingOrders(currentTime): 
+    - Retrieves ongoing orders based on the current time.
+    - Filters orders to find those whose order date is before the current time and delivery date is after the current time.
+
+getOrderHistory(userId): 
+    - Retrieves the order history for a specific user based on the provided user ID.
+*/
 const Datastore = require("nedb");
 const db = new Datastore({
   filename: "./orders.db",
@@ -7,12 +22,11 @@ const { isBefore, add, format } = require("date-fns");
 
 function createOrder(userId, productId, price) {
   return new Promise((resolve, reject) => {
-    // Get the current date and time and format it
     const orderDate = format(new Date(), "yyyy-MM-dd HH:mm");
     const deliveryDate = format(
       add(new Date(), { hours: 2 }),
       "yyyy-MM-dd HH:mm"
-    ); // adds 2 hours to the order date and formats it
+    );
 
     db.insert(
       { userId, productId, price, orderDate, deliveryDate },
