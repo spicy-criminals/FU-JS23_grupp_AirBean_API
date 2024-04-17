33ed1af2cb0bd920e0aebdc326dbd152879ea649
daniel-budai <danielbudai@tutanota.com>
2024-04-17 12:25:16 +0200

    added timestamp, date formating

--- repositories/orderRepository.js
+++ repositories/orderRepository.js
@@ -3,12 +3,19 @@ const db = new Datastore({
   filename: "./orders.db",
   autoload: true,
 });
-const { isBefore } = require("date-fns");
+const { isBefore, add, format } = require("date-fns");
 
 function createOrder(userId, productId, price) {
   return new Promise((resolve, reject) => {
+    // Get the current date and time and format it
+    const orderDate = format(new Date(), "yyyy-MM-dd HH:mm");
+    const deliveryDate = format(
+      add(new Date(), { hours: 2 }),
+      "yyyy-MM-dd HH:mm"
+    ); // adds 2 hours to the order date and formats it
+
     db.insert(
-      { userId, productId, price, timestamp: new Date() },
+      { userId, productId, price, orderDate, deliveryDate },
       (err, newDoc) => {
         if (err) reject(err);
         else resolve(newDoc);
@@ -23,8 +30,12 @@ function getOngoingOrders(currentTime) {
       if (err) reject(err);
       else {
         const filteredOngoingOrders = docs.filter((order) => {
-          const orderTime = new Date(order.timestamp);
-          return isBefore(orderTime, currentTime);
+          const orderTime = new Date(order.orderDate);
+          const deliveryTime = new Date(order.deliveryDate);
+          return (
+            isBefore(orderTime, currentTime) &&
+            isBefore(currentTime, deliveryTime)
+          );
         });
         resolve(filteredOngoingOrders);
       }
